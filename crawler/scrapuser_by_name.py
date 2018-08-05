311148898af5cb24746b01c1fe8b685f45fd16d5
Imam Maulana Akbar Rachbini <imrachbini@gmail.com>
2018-08-05 23:57:40 +0700

    handle out of index error with try catch

--- crawler/scrapuser_by_name.py
+++ crawler/scrapuser_by_name.py
@@ -44,21 +44,26 @@ def get_all_tweets(screen_name):
     # oldest = 826569136390144000 - 1
     # keep grabbing tweets until there are no tweets left to grab
     while len(new_tweets) > 0:
-        print("getting tweets before %s" % (oldest))
+        print("INFO:getting tweets before %s" % (oldest))
+
+        # save most recent tweets
+        alltweets.extend(new_tweets)
+
+        # update the id of the oldest tweet less one
+        print('INFO:Scrapping user ' + screen_name)
+        print("INFO:%s tweets downloaded so far" % (len(alltweets)))
+
+        try:
+            oldest = alltweets[-1].id - 1
+        except Exception as e:
+            logging.error(e)
+            break
 
         # all subsiquent requests use the max_id param to prevent duplicates
         new_tweets = api.user_timeline(
             screen_name=screen_name,
             count=200, max_id=oldest
         )
-
-        # save most recent tweets
-        alltweets.extend(new_tweets)
-
-        # update the id of the oldest tweet less one
-        oldest = alltweets[-1].id - 1
-
-        print("...%s tweets downloaded so far" % (len(alltweets)))
 
     # transform the tweepy tweets into a 2D array that will populate the csv
     outtweets = [[
